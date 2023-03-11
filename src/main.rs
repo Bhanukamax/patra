4157d981add597893e5b1db475a675dc3d22ed70
Bhanuka <bhanukamax@gmail.com>
2023-03-11 17:43:30 +0530

    create a struct to hold app state

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,47 @@ use termion::raw::IntoRawMode;
 use termion::screen::{AlternateScreen, IntoAlternateScreen};
 use termion::{color, style};
 
+struct FileList<'a> {
+    items: Vec<&'a str>,
+    c_idx: u16,
+}
+
+impl<'a> FileList<'a> {
+    fn next(&mut self) {
+        // self.c_idx = (self.c_idx + 1) % self.items.len() as u16
+        if self.c_idx == self.items.len() as u16 {
+            self.c_idx = 1;
+        } else {
+            self.c_idx = self.c_idx + 1
+        }
+    }
+
+    fn prev(&mut self) {
+        if self.c_idx == 1 {
+            self.c_idx = self.items.len() as u16;
+        } else {
+            self.c_idx = self.c_idx - 1;
+        }
+    }
+}
+
 fn main() {
     let mut screen = stdout().into_alternate_screen().unwrap();
     let mut stdout = stdout().into_raw_mode().unwrap();
 
+    let items = vec![
+        "word1", "word2", "word3", "word4", "word5", "word6", "word7",
+    ];
+
+    let mut file_list = FileList {
+        items: items.clone(),
+        c_idx: 1,
+    };
+
     print!("{}", termion::clear::All);
     write!(screen, "{}", termion::cursor::Goto(1, 1)).unwrap();
 
-    let mut c_idx : u16= 3;
-    render(&mut screen, c_idx);
+    render(&mut screen, &file_list);
     stdout.flush().unwrap();
 
     let stdin = stdin();
@@ -27,29 +59,27 @@ fn main() {
 
         match evt {
             Event::Key(Key::Char('q')) => break,
-            Event::Key(Key::Char('j')) => c_idx += 1,
-            Event::Key(Key::Char('k')) => c_idx -= 1,
+            Event::Key(Key::Char('j')) => file_list.next(),
+            Event::Key(Key::Char('k')) => file_list.prev(),
             _ => {}
         }
-        render(&mut screen, c_idx);
+        render(&mut screen, &file_list);
         screen.flush().unwrap();
         stdout.flush().unwrap();
     }
     write!(screen, "{}", termion::clear::All).unwrap();
 }
 
-fn render<W: Write>(screen: &mut AlternateScreen<W>, c_idx: u16) {
-    let items = [
-        "word1", "word2", "word3", "word4", "word5", "word6", "word7",
-    ];
+fn render<W: Write>(screen: &mut AlternateScreen<W>, file_list: &FileList) {
     let mut idx = 1;
-    for item in items {
-        if c_idx == idx {
+    for item in file_list.items.clone() {
+        if file_list.c_idx == idx {
+            write!(screen, "{} ", termion::cursor::Goto(1, idx)).unwrap();
             set_style_alt(screen);
-            write!(screen, "{}{}", termion::cursor::Goto(1, idx), item).unwrap();
+            write!(screen, "{}", item).unwrap();
             set_style_main(screen);
         } else {
-            write!(screen, "{}{}", termion::cursor::Goto(1, idx), item).unwrap();
+            write!(screen, "{} {}", termion::cursor::Goto(1, idx), item).unwrap();
         }
         idx += 1;
     }
