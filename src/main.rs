8f2d1a8ff7a61fd29d5adec4e2b7140fe5c80d54
Bhanuka <bhanukamax@gmail.com>
2023-03-15 08:58:16 +0530

    feat: abstract file item renderer and added more asthetics

--- src/main.rs
+++ src/main.rs
@@ -219,41 +219,51 @@ fn main() {
 
 fn render<W: Write>(screen: &mut AlternateScreen<W>, file_list: &Vec<FileItem>, c_idx: u16) {
     let mut idx = 1;
+    for item in file_list.clone() {
+        render_item(screen, &item, idx, c_idx == idx);
+        idx += 1;
+    }
+}
 
+fn render_item<W: Write>(
+    screen: &mut AlternateScreen<W>,
+    item: &FileItem,
+    idx: u16,
+    selected: bool,
+) {
     let file_icon = "";
     let folder_icon = "";
     let sym_icon = "";
     let unknown_icon = "";
-
-    for item in file_list.clone() {
-        let icon = match item.file_type {
-            FileItemType::Dir => folder_icon,
-            FileItemType::File => file_icon,
-            FileItemType::Sym => sym_icon,
-            FileItemType::Unknown => unknown_icon,
-        };
-
-        match item.file_type {
-            FileItemType::Dir => set_style_dir(screen),
-            _ => set_style_file(screen),
+    let icon = match item.file_type {
+        FileItemType::Dir => folder_icon,
+        FileItemType::File => file_icon,
+        FileItemType::Sym => sym_icon,
+        FileItemType::Unknown => unknown_icon,
+    };
+    let mut suffix = "";
+    match item.file_type {
+        FileItemType::Dir => {
+            suffix = "/";
+            set_style_dir(screen)
         }
-
-        if c_idx == idx {
-            write!(screen, "{}{} ", termion::cursor::Goto(1, idx + 2), icon).unwrap();
-            set_style_alt(screen);
-            write!(screen, "{}", item.name).unwrap();
-            set_style_main(screen);
-        } else {
-            write!(
-                screen,
-                "{}{} {}",
-                termion::cursor::Goto(1, idx + 2),
-                icon,
-                item.name.to_string()
-            )
-            .unwrap();
-        }
-        idx += 1;
+        _ => set_style_file(screen),
+    }
+    if selected {
+        write!(screen, "{}{} ", termion::cursor::Goto(1, idx + 2), icon).unwrap();
+        set_style_alt(screen);
+        write!(screen, "{}{}", item.name, suffix).unwrap();
+        set_style_main(screen);
+    } else {
+        write!(
+            screen,
+            "{}{} {}{}",
+            termion::cursor::Goto(1, idx + 2),
+            icon,
+            item.name.to_string(),
+            suffix
+        )
+        .unwrap();
     }
 }
 
