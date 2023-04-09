86330aac7736ebf083d3e68cda387f5d1de393db
Bhanuka <bhanukamax@gmail.com>
2023-04-09 11:20:26 +0530

    refactor: move all screen write fn's to Display impl

--- src/display.rs
+++ src/display.rs
@@ -1,5 +1,4 @@
 use crate::app::{PatraFileItemType, PatraFileListItem, PatraFileState};
-// use std::io::Write;
 use std::io::{stdout, Write};
 use termion::screen::IntoAlternateScreen;
 use termion::{self, color, screen::AlternateScreen, style};
@@ -18,7 +17,8 @@ impl Display {
         self.screen.flush()
     }
     pub fn render(&mut self, state: &PatraFileState) -> Result<(), std::io::Error> {
-        render(&mut self.screen, state)?;
+        self.render_path(state)?;
+        self.render_app(&state.list.clone(), state.c_idx)?;
         Ok(())
     }
     pub fn hide_cursor(&mut self) -> Result<(), std::io::Error> {
@@ -27,91 +27,80 @@ impl Display {
     pub fn show_cursor(&mut self) -> Result<(), std::io::Error> {
         write!(self.screen, "{} ", termion::cursor::Show)
     }
-}
 
-pub fn render<W: Write>(
-    screen: &mut AlternateScreen<W>,
-    state: &PatraFileState,
-) -> Result<(), std::io::Error> {
-    render_path(screen, state)?;
-    render_app(screen, &state.list.clone(), state.c_idx)?;
-    Ok(())
-}
-
-pub fn render_app<W: Write>(
-    screen: &mut AlternateScreen<W>,
-    file_list: &[PatraFileListItem],
-    c_idx: u16,
-) -> Result<(), std::io::Error> {
-    file_list.iter().enumerate().for_each(|(idx, item)| {
-        render_item(screen, item, idx as u16 + 1, c_idx == idx as u16 + 1).unwrap()
-    });
-    Ok(())
-}
-
-pub fn render_path<W: Write>(
-    screen: &mut AlternateScreen<W>,
-    file_list: &PatraFileState,
-) -> Result<(), std::io::Error> {
-    set_style_path(screen);
-    write!(screen, "{}", termion::clear::All)?;
-    move_cursor_cursor(screen, 10, 1);
-    write!(screen, "                   ")?;
-    move_cursor_cursor(screen, 10, 1);
-    write!(screen, "{} ", &file_list.path)?;
-    Ok(())
-}
-
-pub fn render_item<W: Write>(
-    screen: &mut AlternateScreen<W>,
-    item: &PatraFileListItem,
-    idx: u16,
-    selected: bool,
-) -> Result<(), std::io::Error> {
-    set_style_file(screen);
-    let (icon, suffix) = match item.file_type {
-        PatraFileItemType::Dir => {
-            set_style_dir(screen);
-            ("", "/")
-        }
-        PatraFileItemType::File => ("", ""),
-        PatraFileItemType::Sym => ("", ""),
-        PatraFileItemType::Unknown => ("⚠", ""),
-    };
-
-    if selected {
-        write!(screen, "{}", style::Bold)?;
-        move_cursor_cursor(screen, 1, idx + 2);
-        write!(screen, "{} ", icon)?;
-        write!(screen, "{}", style::Underline)?;
-        write!(screen, "{}{}", item.name, suffix)?;
-        write!(screen, "{}", style::NoBold)?;
-        write!(screen, "{}", style::NoUnderline)?;
-    } else {
-        move_cursor_cursor(screen, 1, idx + 2);
-        write!(screen, "{}", style::Bold)?;
-        write!(screen, "{}", icon)?;
-        write!(screen, "{}", style::NoBold)?;
-        write!(screen, " {}{}", item.name, suffix)?;
+    pub fn render_app(
+        &mut self,
+        file_list: &[PatraFileListItem],
+        c_idx: u16,
+    ) -> Result<(), std::io::Error> {
+        file_list.iter().enumerate().for_each(|(idx, item)| {
+            self.render_item(item, idx as u16 + 1, c_idx == idx as u16 + 1)
+                .unwrap()
+        });
+        Ok(())
     }
-    Ok(())
-}
 
-pub fn set_style_dir<W: Write>(screen: &mut AlternateScreen<W>) {
-    write!(screen, "{}", color::Fg(color::Blue)).unwrap();
-    write!(screen, "{}", color::Bg(color::Black)).unwrap();
-}
+    pub fn render_path(&mut self, file_list: &PatraFileState) -> Result<(), std::io::Error> {
+        self.set_style_path();
+        write!(&mut self.screen, "{}", termion::clear::All)?;
+        self.move_cursor_cursor(10, 1);
+        write!(&mut self.screen, "                   ")?;
+        self.move_cursor_cursor(10, 1);
+        write!(&mut self.screen, "{} ", &file_list.path)?;
+        Ok(())
+    }
 
-pub fn set_style_path<W: Write>(screen: &mut AlternateScreen<W>) {
-    write!(screen, "{}", color::Fg(color::Yellow)).unwrap();
-    write!(screen, "{}", color::Bg(color::Black)).unwrap();
-}
+    pub fn render_item(
+        &mut self,
+        item: &PatraFileListItem,
+        idx: u16,
+        selected: bool,
+    ) -> Result<(), std::io::Error> {
+        self.set_style_file();
+        let (icon, suffix) = match item.file_type {
+            PatraFileItemType::Dir => {
+                self.set_style_dir();
+                ("", "/")
+            }
+            PatraFileItemType::File => ("", ""),
+            PatraFileItemType::Sym => ("", ""),
+            PatraFileItemType::Unknown => ("⚠", ""),
+        };
 
-pub fn set_style_file<W: Write>(screen: &mut AlternateScreen<W>) {
-    write!(screen, "{}", color::Fg(color::White)).unwrap();
-    write!(screen, "{}", color::Bg(color::Black)).unwrap();
-}
+        if selected {
+            write!(&mut self.screen, "{}", style::Bold)?;
+            self.move_cursor_cursor(1, idx + 2);
+            write!(&mut self.screen, "{} ", icon)?;
+            write!(&mut self.screen, "{}", style::Underline)?;
+            write!(&mut self.screen, "{}{}", item.name, suffix)?;
+            write!(&mut self.screen, "{}", style::NoBold)?;
+            write!(&mut self.screen, "{}", style::NoUnderline)?;
+        } else {
+            self.move_cursor_cursor(1, idx + 2);
+            write!(&mut self.screen, "{}", style::Bold)?;
+            write!(&mut self.screen, "{}", icon)?;
+            write!(&mut self.screen, "{}", style::NoBold)?;
+            write!(&mut self.screen, " {}{}", item.name, suffix)?;
+        }
+        Ok(())
+    }
 
-pub fn move_cursor_cursor<W: Write>(screen: &mut AlternateScreen<W>, x: u16, y: u16) {
-    write!(screen, "{}", termion::cursor::Goto(x, y)).unwrap();
+    pub fn set_style_dir(&mut self) {
+        write!(&mut self.screen, "{}", color::Fg(color::Blue)).unwrap();
+        write!(&mut self.screen, "{}", color::Bg(color::Black)).unwrap();
+    }
+
+    pub fn set_style_path(&mut self) {
+        write!(&mut self.screen, "{}", color::Fg(color::Yellow)).unwrap();
+        write!(&mut self.screen, "{}", color::Bg(color::Black)).unwrap();
+    }
+
+    pub fn set_style_file(&mut self) {
+        write!(&mut self.screen, "{}", color::Fg(color::White)).unwrap();
+        write!(&mut self.screen, "{}", color::Bg(color::Black)).unwrap();
+    }
+
+    pub fn move_cursor_cursor(&mut self, x: u16, y: u16) {
+        write!(&mut self.screen, "{}", termion::cursor::Goto(x, y)).unwrap();
+    }
 }
